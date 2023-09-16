dca2e35ed6247f7955dc7fa84a97b4f1d21659a9
Friedrich Weinmann <FriedrichWeinmann@users.noreply.github.com>
2023-09-16 12:32:03 +0200

    Update P3-AST.ps1

--- P3-AST.ps1
+++ P3-AST.ps1
@@ -6,3 +6,12 @@ $rootPath = 'C:\code\GitHub\P2023-PSSat-DeprecatingAzureADMSOnline'
 $resources = "$rootPath\resources"
 
 code "$resources\Get-DCStatistics.ps1"
+$result = Read-ReAst -Path "$resources\Get-DCStatistics.ps1"
+$result.Ast
+$result.Ast.EndBlock.Statements[0].Body.ProcessBlock
+$result.Ast.EndBlock.Statements[0].Body.ProcessBlock.Extent
+
+Search-ReAst -Ast $result.Ast -Filter {
+	$args[0] -is [System.Management.Automation.Language.VariableExpressionAst] -and
+	$args[0].VariablePath.UserPath -eq 'param'
+}
